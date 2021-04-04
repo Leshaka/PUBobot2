824cb30c2647b6c0e660fc17cf4ace9e075f8611
Leshaka <leshkajm@ya.ru>
2021-04-04 15:36:09 +0300

    check on correct msgids in translation files.

--- compile_locales.sh
+++ compile_locales.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+reference="ru.po"
 
 cd locales;
 for i in *.po;
 do
+  changes=$(diff -u0  <(grep -e '^msgid' $reference) <(grep -e '^msgid' $i) | grep 'msgid');
+  if [ ! -z "$changes" ];
+  then
+    echo "--------"
+    echo "Translation file \"$i\" contains changes! Please fix."
+    echo "$changes";
+    echo "--------"
+  fi
   folder="compiled/${i%%.*}/LC_MESSAGES/";
   mkdir -p $folder;
   msgfmt $i -o "$folder/all.mo";
