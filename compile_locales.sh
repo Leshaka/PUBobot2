c3fa874d0ae043142f2c19664a499c37f901425c
Michał Sochoń <kaszpir@gmail.com>
2021-09-20 14:43:24 +0200

    Run shfmt over shell scripts

--- compile_locales.sh
+++ compile_locales.sh
@@ -2,18 +2,19 @@
 
 reference="ru.po"
 
-cd locales;
-for i in *.po;
-do
-  changes=$(diff -u0  <(grep -e '^msgid' $reference) <(grep -e '^msgid' $i) | grep 'msgid');
-  if [ ! -z "$changes" ];
-  then
+cd locales
+for i in *.po; do
+  echo "Processing $i"
+  changes=$(diff -u0 <(grep -e '^msgid' $reference) <(grep -e '^msgid' "$i") | grep 'msgid')
+  if [ -n "$changes" ]; then
     echo "--------"
     echo "Translation file \"$i\" contains changes! Please fix."
-    echo "$changes";
+    echo "$changes"
     echo "--------"
   fi
-  folder="compiled/${i%%.*}/LC_MESSAGES/";
-  mkdir -p $folder;
-  msgfmt $i -o "$folder/all.mo";
+  folder="compiled/${i%%.*}/LC_MESSAGES/"
+  mkdir -p "$folder"
+  msgfmt "$i" -o "$folder/all.mo"
 done
+
+echo "All done."
