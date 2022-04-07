98053a96d9a29a7a07589ae446b8ee306051bd9f
Leshaka <leshkajm@ya.ru>
2022-04-07 17:12:47 +0300

    update community-es.po make compile_locales.sh check ignore position

--- compile_locales.sh
+++ compile_locales.sh
@@ -5,7 +5,7 @@ reference="ru.po"
 cd locales;
 for i in *.po;
 do
-  changes=$(diff -u0  <(grep -e '^msgid' $reference) <(grep -e '^msgid' $i) | grep 'msgid');
+  changes=$(diff -u0  <(grep -e '^msgid' $reference|sort) <(grep -e '^msgid' $i|sort) | grep 'msgid');
   if [ ! -z "$changes" ];
   then
     echo "--------"
